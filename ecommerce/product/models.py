77dc48c0419c75b94e2d7d9d7d38197ded1c91f4
DevBin <devbin404@gmail.com>
2022-11-03 15:38:05 +0330

    Fix bugs in models and migrate models

--- ecommerce/product/models.py
+++ ecommerce/product/models.py
@@ -2,7 +2,7 @@ from pyexpat import model
 from django.db import models
 
 class Category(models.Model):
-    name = model.CharField(max_length=255)
+    name = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
@@ -10,15 +10,15 @@ class Category(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=255)
     desc = models.TextField(null=True)
-    price = models.DecimalField(decimal_place=2, max_digit=6)
+    price = models.DecimalField(decimal_places=2, max_digits=6)
     image = models.ImageField()
     count = models.IntegerField()
     is_guarantee = models.BooleanField()
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL)
+    category = models.ForeignKey(Category, null=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return self.name
-        
+
 
 
 
